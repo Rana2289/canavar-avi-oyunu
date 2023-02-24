48d4ae17c5226defc352aa50c539116dbb7ae0dd
ranakaya2289@gmail.com <ranakaya2289@gmail.com>
2023-02-24 20:32:04 +0300

    kullanma hakkı

--- app.js
+++ app.js
@@ -1,75 +1,88 @@
 new Vue({
     el: "#app",
-    data :{
-        player_heal : 100,
-        monster_heal:100,
-        game_is_on :false,
-        logs : [],
+    data: {
+        player_heal: 100,
+        monster_heal: 100,
+        game_is_on: false,
+        logs: [],
+        hak: 100,
     },
-    methods:{
-        start_game :function (){
-            this.game_is_on=true;
-            this.player_heal=100;
-            this.monster_heal=100;
+    methods: {
+        start_game: function () {
+            this.game_is_on = true;
+            this.player_heal = 100;
+            this.monster_heal = 100;
         },
-        attack :function (){
-            var sayi =Math.ceil(Math.random()*10);
-            this.monster_heal-=sayi;
+        attack: function () {
+            var sayi = Math.ceil(Math.random() * 10);
+            this.monster_heal -= sayi;
             this.monster_attack();
-            this.add_to_log({turn  : "p" ,text :"oyuncu atağı (" + sayi +") " })
+            this.add_to_log({turn: "p", text: "oyuncu atağı (" + sayi + ") "})
+            this.hak += 5;
+        },
+        special_attack: function () {
+            if (this.hak >= 15) {
+                var sayi = Math.ceil(Math.random() * 25);
+                this.monster_heal -= sayi;
+                this.monster_attack();
+                this.add_to_log({turn: "p", text: " özel oyuncu atağı (" + sayi + ") "})
+                this.hak -= 15;
+            }
+        },
+        heal_up: function () {
+            if (this.hak >= 10) {
+                var sayi = Math.ceil(Math.random() * 25);
+                this.player_heal += sayi;
+                this.monster_attack();
+                this.add_to_log({turn: "p", text: " ilk yardım (" + sayi + ") "})
+                this.hak -= 10;
+            }
+        },
+        give_up: function () {
+            this.player_heal = 0;
+            this.add_to_log({turn: "p", text: " oyuncu pes etti!!! "})
+        },
 
+        monster_attack: function () {
+            var sayi = Math.ceil(Math.random() * 15);
+            this.player_heal -= sayi;
+            this.add_to_log({turn: "m", text: " canavar atağı  (" + sayi + ") "})
         },
-        special_attack:function (){
-            var sayi =Math.ceil(Math.random()*25);
-            this.monster_heal-=sayi;
-            this.monster_attack();
-            this.add_to_log({turn  : "p" ,text :" özel oyuncu atağı (" + sayi +") "})
-
-        },
-        heal_up:function (){
-            var sayi =Math.ceil(Math.random()*25);
-            this.player_heal+=sayi;
-            this.monster_attack();
-            this.add_to_log({turn  : "p" ,text :" ilk yardım (" + sayi +") "})
-        },
-        give_up : function (){
-            this.player_heal=0;
-            this.add_to_log({turn  : "p" ,text :" oyuncu pes etti!!! "})
-        },
-
-        monster_attack: function (){
-            var sayi =Math.ceil(Math.random()*15);
-            this.player_heal-=sayi;
-            this.add_to_log({turn  : "m" ,text :" canavar atağı  (" + sayi +") "})
-        },
-        add_to_log :function (log){
+        add_to_log: function (log) {
             this.logs.push(log);
         },
     },
-    watch :{
-        player_heal : function (value) {
-            if(value<=0){
-                this.player_heal=0
-                if(confirm("KAYBETTINIZ TEKRAR OYNAMAK ISTERMISINIZZ?")){
-                    this.player_heal= 100;
-                    this.monster_heal=100;
+    watch: {
+        player_heal: function (value) {
+            if (value <= 0) {
+                this.player_heal = 0
+                if (confirm("KAYBETTINIZ TEKRAR OYNAMAK ISTERMISINIZZ?")) {
+                    this.player_heal = 100;
+                    this.monster_heal = 100;
+                    this.hak= 100;
                     this.logs = [];
                 }
-            }
-            else if(value>=100){
-                this.player_heal=100
+            } else if (value >= 100) {
+                this.player_heal = 100
             }
         },
-        monster_heal : function ( value ){
-            if(value<=0){
-                this.monster_heal=0
-                if(confirm("KAZANDINIZ TEKRAR OYNAMAK ISTERMISINIZZ?")){
-                    this.player_heal= 100;
-                    this.monster_heal=100;
+        monster_heal: function (value) {
+            if (value <= 0) {
+                this.monster_heal = 0
+                if (confirm("KAZANDINIZ TEKRAR OYNAMAK ISTERMISINIZZ?")) {
+                    this.player_heal = 100;
+                    this.monster_heal = 100;
+                    this.hak= 100;
                     this.logs = [];
                 }
             }
 
-        }
+        },
+        hak: function (x) {
+            if(x>=100){
+                this.hak=100;
+            }
+
+        },
     }
 })
